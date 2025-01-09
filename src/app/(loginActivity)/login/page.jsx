037a2775187ff432a638a042a478470cce3172fd
saikrishnaM27 <saikrishnamaggidi27@gmail.com>
2025-01-09 10:48:57 +0530

    mnth login design modified
    Signed-off-by: saikrishnaM27 <saikrishnamaggidi27@gmail.com>

--- src/app/(loginActivity)/login/page.jsx
+++ src/app/(loginActivity)/login/page.jsx
@@ -1,24 +1,30 @@
 const Login = () => {
     return (
-        <div className="d-flex justify-content-center align-items-center h-100">
-            <div id="loginPage" className="mx-1 card w-100 py-3">
-                <h2 className="text-center fw-bolder">Login</h2>
-                <form className="px-3 my-3">
-                    <div className="mb-3">
-                        <label htmlFor="mobile-number" className="form-label fw-semibold">Phone Number</label>
-                        <input type="tel" className="form-control p-2" id="mobile-number" name="mobile-number" placeholder="Enter Mobile Number" required pattern="[0-9]{10}" minLength={10} maxLength={10}/>
+        <>
+            <div className="bg-dark text-white p-3"><h1>LOGO</h1></div>
+            <div className="h-90 d-flex justify-content-center align-items-center">
+                <div className="card mx-2 w-100 py-3" id="login-page">
+                    <h2 className="text-center fw-bolder">Register Account</h2>
+                    <div className="d-flex justify-content-center align-items-center px-2 gap-1 my-3">
+                        <div className="btn btn-success p-2 w-100 fw-bold">Sign Up</div>
+                        <div className="btn btn-success p-2 w-100 fw-bold">Login</div>
                     </div>
-                    <div className="mb-3">
-                        <label htmlFor="password" className="form-label fw-semibold">Enter Password</label>
-                        <input type="password" className="form-control p-2" id="password" name="password" placeholder="Enter Password"/>
-                    </div>
-                    <div className="mb-3">
-                        <button type="submit" className="btn btn-success w-100 p-2 fw-bold">Sign in</button>
-                    </div>
-                    <div className="my-2">Don't have an account? <a href="/signup" className="text-decoration-none fw-semibold">Sign Up</a></div>
-                </form>
+                    <form action="" className="my-3 px-3">
+                        <div className="mb-3">
+                            <label htmlFor="mobile-number" className="form-label fw-semibold">Phone Number</label>
+                            <input type="tel" className="form-control p-2 my-1" id="mobile-number" name="mobile-number" placeholder="Enter Mobile Number" required pattern="[0-9]{10}" minLength={10} maxLength={10} />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="password" className="form-label fw-semibold">Enter Password</label>
+                            <input type="password" className="form-control p-2 my-1" id="password" name="password" placeholder="Enter Password" required />
+                        </div>
+                        <div className="">
+                            <button type="submit" className="btn btn-dark w-100 p-2 fw-bold">Register Now</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
--- src/app/(loginActivity)/signup/page.jsx
+++ src/app/(loginActivity)/signup/page.jsx
@@ -1,31 +1,30 @@
 const Signup = () => {
-    'use client'
-
     return (
-        <div className="d-flex justify-content-center align-items-center h-100">
-            <div id="loginPage" className="mx-1 card w-100 py-3">
-                <h2 className="text-center fw-bolder">Sign Up</h2>
-                <form className="px-3 my-3" method="">
-                    <div className="mb-3">
-                        <label htmlFor="mobile-number" className="form-label fw-semibold">Phone Number</label>
-                        <input type="tel" className="form-control p-2" id="mobile-number" name="mobile-number" placeholder="Enter Mobile Number" required pattern="[0-9]{10}" minLength={10} maxLength={10}/>
+        <>
+            <div className="bg-dark text-white p-3"><h1>LOGO</h1></div>
+            <div className="h-90 d-flex justify-content-center align-items-center">
+                <div className="card mx-2 w-100 py-3" id="login-page">
+                    <h2 className="text-center fw-bolder">Register Account</h2>
+                    <div className="d-flex justify-content-center align-items-center px-2 gap-1 my-3">
+                        <div className="btn btn-success p-2 w-100 fw-bold">Sign Up</div>
+                        <div className="btn btn-success p-2 w-100 fw-bold">Login</div>
                     </div>
-                    <div className="mb-3">
-                        <label htmlFor="password" className="form-label fw-semibold">Enter Password</label>
-                        <input type="password" className="form-control p-2" id="password" name="password" placeholder="Enter Password" required/>
-                    </div>
-                    <div className="mb-3">
-                        <button type="submit" className="btn btn-success w-100 p-2 fw-bold">Sign Up</button>
-                    </div>
-                    <div className="my-2">Already have an account? <a href="/login" className="text-decoration-none fw-semibold">Login</a></div>
-                </form>
+                    <form action="" className="my-3 px-3">
+                        <div className="mb-3">
+                            <label htmlFor="mobile-number" className="form-label fw-semibold">Phone Number</label>
+                            <input type="tel" className="form-control p-2 my-1" id="mobile-number" name="mobile-number" placeholder="Enter Mobile Number" required pattern="[0-9]{10}" minLength={10} maxLength={10} />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="password" className="form-label fw-semibold">Enter Password</label>
+                            <input type="password" className="form-control p-2 my-1" id="password" name="password" placeholder="Enter Password" required />
+                        </div>
+                        <div className="">
+                            <button type="submit" className="btn btn-dark w-100 p-2 fw-bold">Register Now</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
+        </>
     );
-}
-
-export const onSubmitForm = async (formData) => {
-    'use server'
-    console.log("hi")
 }
 export default Signup;
