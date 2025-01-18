6fbd6a35e5ee0151c58725c2f52c640e2c716d20
saikrishnaM27 <saikrishnamaggidi27@gmail.com>
2025-01-18 17:58:34 +0530

    PW: clientside form validation done, pending to divide reusable components
    Signed-off-by: saikrishnaM27 <saikrishnamaggidi27@gmail.com>

--- src/app/(loginActivity)/register/page.jsx
+++ src/app/(loginActivity)/register/page.jsx
@@ -4,13 +4,14 @@ import Loading from "app/components/loading";
 import { useState } from "react";
 import Link from 'next/link'
 import { registerAuth } from "app/actions/registerAuth";
+import { signUpFormSchema } from "app/lib/validations";
 
 const Signup = () => {
 
     const [phoneNumber, setPhoneNumber] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPass, setConfirmPass] = useState("");
-    const [invitedBy, setInvitedBy] = useState("");
+    const [invitedBy, setInvitedBy] = useState(undefined);
     const [message, setMessage] = useState("");
     const [passMatch, setPassMatch] = useState(false);
     const [status, setStatus] = useState(false);
@@ -26,27 +27,44 @@ const Signup = () => {
         setLoading(false);
     }
 
-    const checkPass = (e) => {
-        if (password === e.target.value) {
-            setPassMatch(true);
-        } else {
-            setPassMatch(false);
-        }
+    const checkFormBS = () => {
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (() => {
+            'use strict'
+
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            const form = document.querySelector('.needs-validation')
+
+            // Loop over them and prevent submission
+
+            form.addEventListener('submit', event => {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+
+                }
+                form.classList.add('was-validated');
+            }, false)
+        })()
     }
 
     const handleSubmit = async (e) => {
+        checkFormBS();
         e.preventDefault();
         setLoading(true);
-
-        if (!passMatch) {
-            setStatus(false);
+        console.log(invitedBy)
+        const { success, data, error } = signUpFormSchema.safeParse({ phoneNumber, password, confirmPass, invitedBy })
+        if (!success) {
+            error.errors.forEach((error) => {
+                console.log(`Field: ${error.path.join(".")} - Error: ${error.message}`);
+                // console.log(error.path.includes("password"))
+            });
             setLoading(false);
-            setMessage("Both passwords not matched.");
             return;
         }
 
         try {
-            const resp = await registerAuth({ phoneNumber, password, confirmPass, invitedBy });
+            const resp = await registerAuth({ phoneNumber: data.phoneNumber, password: data.password, confirmPass: data.confirmPass, invitedBy: data.invitedBy });
 
             if (resp.status) {
                 setStatus(resp.status);
@@ -75,32 +93,35 @@ const Signup = () => {
                         <Link href="/register" className="btn btn-outline-success w-100 py-2 fw-bold disabled" role="button">Register</Link>
                         <Link href="/login" className="btn btn-success w-100 py-2 fw-bold" role="button">Login</Link>
                     </div>
-                    <form onSubmit={handleSubmit} className="my-2 px-3">
+                    <form onSubmit={handleSubmit} className="my-2 px-3 needs-validation" noValidate>
                         <div className="mb-2">
                             <label htmlFor="phone-number" className="form-label fw-semibold">Phone Number</label>
-                            <input type="tel" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} className="form-control p-2 my-1 fw-semibold" id="phone-number" name="phone-number" placeholder="Enter Phone Number" required pattern="[0-9]{10}" minLength={10} maxLength={10} />
+                            <div className="input-group my-1">
+                                <span className="input-group-text px-2 fw-semibold">+91</span>
+                                <input type="tel" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} className="form-control p-2 fw-semibold" id="phone-number" name="phone-number" placeholder="Enter Phone Number" required pattern="[0-9]{10}" minLength={10} maxLength={10} />
+                            </div>
+                            <div className="invalid-feedback">{message}</div>
                         </div>
                         <div className="mb-2">
                             <label htmlFor="password" className="form-label fw-semibold">Password</label>
-                            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control p-2 my-1  fw-semibold" id="password" name="password" placeholder="Enter Password" required />
+                            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control p-2 my-1 was-validated fw-semibold" id="password" name="password" placeholder="Enter Password" required />
+                            <div className="invalid-feedback">{message}</div>
                         </div>
                         <div className="mb-2">
                             <label htmlFor="confirm-password" className="form-label fw-semibold">Confirm Password</label>
-                            <input type="password" value={confirmPass} onChange={(e) => { setConfirmPass(e.target.value); checkPass(e) }} className={`form-control p-2 my-1 fw-semibold`} id="confirm-password" name="confirm-password" placeholder="Confirm Password" required />
+                            <input type="password" value={confirmPass} onChange={(e) => setConfirmPass(e.target.value)} className={`form-control p-2 my-1 fw-semibold`} id="confirm-password" name="confirm-password" placeholder="Confirm Password" required />
+                            <div className="invalid-feedback">{message}</div>
                         </div>
                         <div className="mb-2">
                             <label htmlFor="invitedBy" className="form-label fw-semibold">Referral Code</label>
                             <input type="text" value={invitedBy} onChange={(e) => setInvitedBy(e.target.value)} className="form-control p-2 my-1 fw-semibold" id="invitedBy" name="invitedBy" placeholder="(optional)" />
+                            <div className="invalid-feedback">{message}</div>
                         </div>
                         <div id="error-msg" className={`fw-semibold m-1 ${status ? "text-success" : "text-danger"}`}>{message}</div>
                         <div className="">
                             <button type="submit" className="btn btn-dark w-100 p-2 fw-bold">Register Now</button>
                         </div>
                     </form>
-                    {/* <div id="google-signin" className="text-center px-3">
-                        <div className="fw-semibold my-1">OR</div>
-                        <button className="btn btn-dark w-100 py-2 fw-bold">Google Sign Up</button>
-                    </div> */}
                 </div>
                 {
                     loading ? <Loading /> : ""
---
+++ src/app/lib/validations.js
@@ -0,0 +1,34 @@
+import { z } from "zod"
+import validator from "validator";
+
+export const signUpFormSchema = z.object({
+    phoneNumber: z
+        .string()
+        .trim()
+        .refine((val) => validator.isMobilePhone(val, 'en-IN'), {
+            message: "Invalid phone number!"
+        }),
+    password: z
+        .string()
+        .min(8, { message: 'Must be at least 8 characters long.' })
+        .regex(/[a-z]/, { message: 'Contain at least one lowercase letter.' })
+        .regex(/[A-Z]/, { message: 'Contain at least one uppercase letter.' })
+        .regex(/[0-9]/, { message: 'Contain at least one number.' })
+        .regex(/[^a-zA-Z0-9]/, {
+            message: 'Contain at least one special character.',
+        })
+        .max(32, { message: 'Must be less than 32 characters long.' })
+        .trim(),
+    confirmPass: z
+        .string()
+        .min(8, { message: 'Must be at least 8 characters long.' })
+        .trim(),
+    invitedBy: z
+        .string()
+        .length(6, {message: 'Invalid Referral code.'})
+        .regex(/[A-Z0-9]/,{message: 'Invalid Referral code.'})
+        .trim().optional(),
+}).refine((data) => data.password === data.confirmPass, {
+    message: 'Passwords do not match.',
+    path: ["confirmPassword"],
+});
