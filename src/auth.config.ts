c9a02ee0ed185ac7b682f5b80eb111e40bdfd9d0
saikrishnaM27 <saikrishnamaggidi27@gmail.com>
2025-01-21 10:31:27 +0530

    user id in session solved
    Signed-off-by: saikrishnaM27 <saikrishnamaggidi27@gmail.com>

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -7,20 +7,23 @@ export const authConfig = {
         Credentials({
             credentials: {
                 phoneNumber: { label: "Phone Number", type: "tel" },
-                password: {  label: "Password", type: "password" },
+                password: { label: "Password", type: "password" },
             },
             authorize: async (credentials) => {
                 let user = await getUserFromDb(credentials);
                 if (!user) throw new Error("Invalid Credentials!")
-                return user as any;
+                return user[0] as any;
             }
         }),
     ]
 } satisfies NextAuthConfig;
 
+interface user {
+    id: number;
+}
 declare module "next-auth" {
-    interface User {
-        id?: string;
+    interface User extends user {
+        // id?: string;
         phoneNumber: string;
         password: string;
         referralCode: string;
