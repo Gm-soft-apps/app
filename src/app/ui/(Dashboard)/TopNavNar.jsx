0706a22e5204e3cff28e906194583db987a41b9e
saikrishnaM27 <saikrishnamaggidi27@gmail.com>
2025-01-21 21:58:50 +0530

    signout func client side added
    Signed-off-by: saikrishnaM27 <saikrishnamaggidi27@gmail.com>

--- src/app/ui/(Dashboard)/TopNavNar.jsx
+++ src/app/ui/(Dashboard)/TopNavNar.jsx
@@ -1,11 +1,14 @@
-import { authSignOut } from "app/actions/auth";
+"use client"
+
+// import { authSignOut } from "app/actions/auth";
+import { signOut } from "next-auth/react";
 
 const TopNavBar = () => {
     return (
         <div className="d-flex justify-content-between align-items-center mb-2 bg-white p-1 shadow-lg">
             <div className="fs-1 btn px-2"><i className="bi bi-person-check"></i></div>
             <div><h2>LOGO</h2></div>
-            <div className="fs-1 btn px-2 btn-outline-danger" onClick={authSignOut}><i className="bi bi-power"></i></div>
+            <div className="fs-1 btn px-2 btn-outline-danger" onClick={()=>signOut()}><i className="bi bi-power"></i></div>
         </div>
     );
 }
