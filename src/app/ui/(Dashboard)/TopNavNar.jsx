dce0c7cba335cb704800ec19d07c209e44c3b178
saikrishnaM27 <saikrishnamaggidi27@gmail.com>
2025-01-22 07:22:07 +0530

    signout arrow fun removed
    Signed-off-by: saikrishnaM27 <saikrishnamaggidi27@gmail.com>

--- src/app/ui/(Dashboard)/TopNavNar.jsx
+++ src/app/ui/(Dashboard)/TopNavNar.jsx
@@ -1,6 +1,5 @@
 "use client"
 
-// import { authSignOut } from "app/actions/auth";
 import { signOut } from "next-auth/react";
 
 const TopNavBar = () => {
@@ -8,7 +7,7 @@ const TopNavBar = () => {
         <div className="d-flex justify-content-between align-items-center mb-2 bg-white p-1 shadow-lg">
             <div className="fs-1 btn px-2"><i className="bi bi-person-check"></i></div>
             <div><h2>LOGO</h2></div>
-            <div className="fs-1 btn px-2 btn-outline-danger" onClick={()=>signOut()}><i className="bi bi-power"></i></div>
+            <div className="fs-1 btn px-2 btn-outline-danger" onClick={signOut}><i className="bi bi-power"></i></div>
         </div>
     );
 }
