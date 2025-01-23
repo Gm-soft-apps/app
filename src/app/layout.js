2b4bc1c4998185026b31a5a567f5b1874b97210f
saikrishnaM27 <saikrishnamaggidi27@gmail.com>
2025-01-23 12:59:49 +0530

    meta viewport updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,13 +2,15 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../../node_modules/bootstrap-icons/font/bootstrap-icons.min.css'
 import './globals.css'
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-      </head>
       <body className='bg-secondary-subtle'>
         <div id='root' className='d-sm-none'>{children}</div>
         <div id='no-desktop' className='h-100 d-none d-sm-flex justify-content-center align-items-center'><h2><span className='bg-white text-dark px-2'>Please Visit in</span><span className='bg-dark text-white px-2'>Mobile Phone</span></h2></div>
