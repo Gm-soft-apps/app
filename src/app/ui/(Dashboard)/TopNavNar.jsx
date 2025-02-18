282be4c49ec2f5a1d5eaf5b624b921451caf20e6
spidy-verse <eliteanswers.in@gmail.com>
2025-02-18 18:17:35 +0530

    pw: profile page created

--- src/app/ui/(Dashboard)/TopNavNar.jsx
+++ src/app/ui/(Dashboard)/TopNavNar.jsx
@@ -1,11 +1,12 @@
 "use client"
 
+import Link from "next/link";
 import SignoutModal from "../(AuthForm)/SignoutModal";
 
 const TopNavBar = () => {
     return (
         <div className="d-flex justify-content-between align-items-center mb-2 bg-white p-1 shadow-lg">
-            <div className="fs-1 btn px-2"><i className="bi bi-person-check"></i></div>
+            <Link href="/dashboard/profile" className="fs-1 btn px-2"><i className="bi bi-person-check"></i></Link>
             <div><h2>LOGO</h2></div>
             <div className="fs-1 btn px-2 text-danger" data-bs-toggle="modal" data-bs-target="#signoutModal"><i className="bi bi-box-arrow-right"></i></div>
             <SignoutModal/>
