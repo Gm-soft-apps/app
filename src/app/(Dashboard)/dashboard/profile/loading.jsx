5d80a12601bb2be4405e9cc6af176e4a6d31a356
spidy-verse <eliteanswers.in@gmail.com>
2025-02-22 17:58:42 +0530

    profile page design modified 3
    Signed-off-by: spidy-verse <eliteanswers.in@gmail.com>

--- src/app/(Dashboard)/dashboard/profile/loading.jsx
+++ src/app/(Dashboard)/dashboard/profile/loading.jsx
@@ -2,7 +2,7 @@ const LoadingProfile = () => {
     return (
         <div id="profile" className="mx-1">
             <div className="card p-3">
-                <div className="mb-3">
+                <div className="mb-3 text-center">
                     <img className="rounded-pill placeholder" alt="" height="150px" width="150px" />
                 </div>
                 <div className="placeholder-wave">
