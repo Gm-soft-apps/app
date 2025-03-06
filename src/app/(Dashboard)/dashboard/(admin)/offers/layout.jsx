2cb6c5b39bb4811347e8718d91840bfd509f808b
spidy-verse <eliteanswers.in@gmail.com>
2025-03-06 20:34:02 +0530

    mx-1 added to layout of offers
    Signed-off-by: spidy-verse <eliteanswers.in@gmail.com>

--- src/app/(Dashboard)/dashboard/(admin)/offers/layout.jsx
+++ src/app/(Dashboard)/dashboard/(admin)/offers/layout.jsx
@@ -9,15 +9,15 @@ const OffersLayout = (props) => {
 
     const isAddOffers = path === "/dashboard/offers/add"
     return (
-        <>
-            <div className="text-white text-center fw-bold row gap-1 px-1">
+        <div className="mx-1">
+            <div className="text-white text-center fw-bold row gap-1">
                 <div className={`col py-1 text-decoration-none rounded-1  text-white bg-secondary`}>All</div>
                 <div className={`col py-1 text-decoration-none rounded-1  text-white bg-secondary`}>Active</div>
                 <div className={`col py-1 text-decoration-none rounded-1  text-white bg-secondary`}>Inactive</div>
                 <Link href="/dashboard/offers/add" className={`col py-1 text-decoration-none rounded-1  text-white ${isAddOffers ? "bg-primary" : "bg-secondary"}`}>Add</Link>
             </div>
             {props.children}
-        </>
+        </div>
     );
 }
 
