a78dfeeb9dac1ae13a851e0002b26654631aeceb
spidy-verse <eliteanswers.in@gmail.com>
2025-03-07 21:40:56 +0530

    pw: /dashboard/offers/add page updated
    Signed-off-by: spidy-verse <eliteanswers.in@gmail.com>

--- src/app/(Dashboard)/dashboard/(admin)/offers/add/page.jsx
+++ src/app/(Dashboard)/dashboard/(admin)/offers/add/page.jsx
@@ -1,18 +1,20 @@
 const AddOffer = () => {
     return (
-        <form className="bg-white p-1">
-            <div className="row gap-1">
-                {/* <div className="col"> */}
-                    {/* <label htmlFor="offer-logo" className="form-label fw-semibold">Choose Offer Logo</label> */}
-                    <input type="file" className="col" id="offer-logo" name="offer-logo" required />
-                {/* </div> */}
+        <form className="bg-white p-1 fw-semibold">
+            <h2 className="text-center bg-warning py-1 mb-2">New Offer</h2>
+            <div className="row gap-2">
+                <input type="file" className="col" id="offer-logo" name="offer-logo" required />
                 <div className="col">
-                    <h3>New Offer</h3>
-                    <input type="number" className="form-control fw-semibold my-1 px-2 border rounded-1" id="offer-num" name="offer-num" placeholder="Offer No" required />
-                    <input type="text" className="form-control fw-semibold my-1 px-2 border rounded-1" id="offer-title" name="offer-title" placeholder="Offer Title" required />
-                    <input type="number" className="form-control fw-semibold my-1 px-2 border rounded-1" id="offer-payout" name="offer-payout" placeholder="Offer payout" required />
+                    <input type="number" className="form-control fw-semibold my-1 py-1 px-2 border rounded-1" id="offer-num" name="offer-num" placeholder="Offer No" required />
+                    <input type="text" className="form-control fw-semibold my-1 py-1 px-2 border rounded-1" id="offer-name" name="offer-name" placeholder="Offer name" required />
+                    <input type="number" className="form-control fw-semibold my-1 py-1 px-2 border rounded-1" id="offer-payout" name="offer-payout" placeholder="Offer payout" required />
                 </div>
             </div>
+            <div>
+                <input type="text" className="form-control fw-semibold py-1 px-2 mb-2 border rounded-1" id="offer-title" name="offer-name" placeholder="offer title" />
+            </div> <hr />
+
+            <h2 className="text-center bg-warning py-1 my-1">Offer Details</h2>
         </form>
     );
 }
