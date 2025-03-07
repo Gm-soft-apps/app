d67b84a684bec1c1dfda7a5b731ee6140d652803
spidy-verse <eliteanswers.in@gmail.com>
2025-03-07 20:40:07 +0530

    pw: /dashboard/offers/add page updated
    Signed-off-by: spidy-verse <eliteanswers.in@gmail.com>

--- src/app/(Dashboard)/dashboard/(admin)/offers/layout.jsx
+++ src/app/(Dashboard)/dashboard/(admin)/offers/layout.jsx
@@ -7,11 +7,12 @@ const OffersLayout = (props) => {
 
     const path = usePathname();
 
-    const isAddOffers = path === "/dashboard/offers/add"
+    const isAddOffers = path === "/dashboard/offers/add";
+    const isOffers = path === "/dashboard/offers"
     return (
         <div className="mx-1">
-            <div className="text-white text-center fw-bold row gap-1">
-                <div className={`col py-1 text-decoration-none rounded-1  text-white bg-secondary`}>All</div>
+            <div className="text-white text-center fw-bold row gap-1 my-2">
+                <Link href="/dashboard/offers"     className={`col py-1 text-decoration-none rounded-1  text-white ${isOffers ? "bg-primary" : "bg-secondary"}`}>All</Link>
                 <div className={`col py-1 text-decoration-none rounded-1  text-white bg-secondary`}>Active</div>
                 <div className={`col py-1 text-decoration-none rounded-1  text-white bg-secondary`}>Inactive</div>
                 <Link href="/dashboard/offers/add" className={`col py-1 text-decoration-none rounded-1  text-white ${isAddOffers ? "bg-primary" : "bg-secondary"}`}>Add</Link>
