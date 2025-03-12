6d98d48b33d43abf17771e31279f6038868c08b4
spidy-verse <eliteanswers.in@gmail.com>
2025-03-12 14:28:05 +0530

    user home dashboard updated
    Signed-off-by: spidy-verse <eliteanswers.in@gmail.com>

--- src/app/ui/Dashboard/UserDashboard.jsx
+++ src/app/ui/Dashboard/UserDashboard.jsx
@@ -4,29 +4,34 @@ const UserDashboard = async () => {
 
     const offers = await getAllOffers();
 
-    const categories = ["All", "Instant Deals", "Savings Accounts", "Demat Accounts", "Credit Cards", "Loan Apps"]
+    const categories = ["Instant Deals", "Savings A/C", "Demat A/C", "Credit Cards", "Loan Apps", "All Offers"]
     return (
         <>
             <div id="categories" className="row row-cols-3 my-2 mx-1">
                 {
                     categories.map((category, idx) => {
-                        return <div className="col px-1" key={idx}><div className="btn btn-success w-100 px-2 py-1 my-1 fw-semibold text-truncate">{category}</div></div>
+                        return <div className="col px-1" key={idx}><div className="btn btn-success w-100 px-1 py-1 my-1 fw-semibold text-truncate">{category}</div></div>
                     })
                 }
             </div>
 
-            <div className="text-center my-2"><h2>Trending Offers Today</h2></div>
+            <div className="text-center my-2"><h2>Trending Offers</h2></div>
 
             {
                 offers.map((offer, idx) => {
                     return (
-                        <div className="p-2 bg-white mx-2 rounded my-2 shadow-lg" key={idx}>
-                            <div className="d-flex justify-content-between align-items-center mb-3">
-                                <img src={offer.offerLogo} alt="img" width={65} height={65} className="border" />
-                                <h3 className="w-75 ms-2">{offer.offerName}</h3>
+                        <div className="p-2 bg-white mx-2 rounded my-2 shadow-lg position-relative" key={idx}>
+                            <div className="user-offer-payout bg-warning position-absolute top-0 end-0 px-3 fw-semibold">Earn Flat ₹ {offer.offerPayout}</div>
+                            <div className="row">
+                                <div className="col-3">
+                                    <img src={offer.offerLogo} alt={offer.offerName} width={65} height={65}/>
+                                </div>
+                                <div className="col align-self-center">
+                                    <h2 className="fs-4 mt-3">{offer.offerName}</h2>
+                                </div>
+                                    <div className="fs-6 fw-semibold mt-1">{offer.offerTitle}</div>
                             </div>
-                            <div className="my-2">{offer.offerTitle}</div>
-                            <button className="btn btn-success p-2 w-100 fw-semibold">Complete Now</button>
+                            <button className="btn btn-primary py-1 mt-1 w-100 fw-semibold">Get Now</button>
                         </div>
                     );
                 })
