cbbdcb060682a0c38d950f35854ad2d6a3236938
spidy-verse <eliteanswers.in@gmail.com>
2025-03-20 21:01:48 +0530

    offerPage modified
    Signed-off-by: spidy-verse <eliteanswers.in@gmail.com>

--- src/app/(Dashboard)/dashboard/offer/[id]/page.jsx
+++ src/app/(Dashboard)/dashboard/offer/[id]/page.jsx
@@ -4,14 +4,14 @@ import { getOfferByID } from "db/offers/handler";
 
 const OfferPage = async ({ params }) => {
     const { id } = await params;
-
     const offer = await getOfferByID(id);
+
     return (
-        <div className="px-1">
+        <div className="px-1 bg-white">
             <div id="offer-banner">
-                <img src={offer.offerBanner} className="w-100 shadow" alt={offer.offerName} height={180} />
+                <img src={offer.offerBanner} className="w-100 shadow-lg" alt={offer.offerName} height={180} />
             </div>
-            <div className="bg-white my-2 px-1 py-1 row gap-1">
+            <div className="bg-white mb-2 px-1 py-1 row gap-1 border">
                 <button className="col btn btn-dark fw-semibold w-100 py-1 rounded-1">Self Link</button>
                 <button className="col btn btn-dark fw-semibold w-100 py-1 rounded-1">Share Link</button>
                 <div className="text-center my-2 fw-semibold">https://gm-app.netlify.app</div>
@@ -19,21 +19,31 @@ const OfferPage = async ({ params }) => {
                     <button className="col-5 btn btn-dark fw-semibold">Copy</button>
                     <button className="col-5 btn btn-dark fw-semibold">Share</button>
                 </div>
-            </div>
+            </div><hr/>
 
-            <div id="offerDetails">
-                <h3>Offer Details</h3>
-                <div>{offer.offerDetails}</div>
-            </div>
+            <div id="offerDetails" className="my-2">
+                <h3 className="fs-6 my-1">Offer Details</h3>
+                <div dangerouslySetInnerHTML={{__html: offer.offerDetails}}></div>
+            </div><hr/>
 
-            <section id="offer-steps">
-                <h3>Offer Steps</h3>
-                <div>{offer.offerSteps}</div>
-            </section>
+            <section id="offer-steps" className="my-2">
+                <h3 className="fs-6 my-1">Offer Steps</h3>
+                <div dangerouslySetInnerHTML={{__html: offer.offerSteps || "N/A"}}></div>
+            </section><hr/>
 
-            <section id="offer-terms">
-                <h3>Offer Terms</h3>
-                <div>{offer.offerTerms}</div>
+            <section id="offer-terms" className="my-2">
+                <h3 className="fs-6 my-1">Offer Terms</h3>
+                <div dangerouslySetInnerHTML={{__html: offer.offerTerms || "N/A"}}></div>
+            </section><hr/>
+
+            <section id="offer-docs" className="my-2">
+                <h3 className="fs-6 my-1">Offer Documents</h3>
+                <div dangerouslySetInnerHTML={{__html: offer.offerDocs || "N/A"}}></div>
+            </section><hr/>
+
+            <section id="offer-payout-rules" className="my-2">
+                <h3 className="fs-6 my-1">Offer Payout Rules</h3>
+                <div dangerouslySetInnerHTML={{__html: offer.offerPayoutRules || "N/A"}}></div>
             </section>
         </div>
     );
