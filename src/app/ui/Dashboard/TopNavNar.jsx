18bd3b1be9c94793fa331dcfde56334d19265685
spidy-verse <eliteanswers.in@gmail.com>
2025-03-30 20:43:51 +0530

    topnavbar position sticky modified
    Signed-off-by: spidy-verse <eliteanswers.in@gmail.com>

--- src/app/ui/Dashboard/TopNavNar.jsx
+++ src/app/ui/Dashboard/TopNavNar.jsx
@@ -10,7 +10,7 @@ const TopNavBar = () => {
     const btnText = "YES";
 
     return (
-        <div className="d-flex justify-content-between align-items-center mb-2 bg-white p-1 shadow-lg">
+        <div className="d-flex justify-content-between align-items-center mb-2 bg-white p-1 shadow-lg border-secondary border-bottom position-sticky sticky-top">
             <Link href="/dashboard/account" className="fs-1 btn px-2"><i className="bi bi-person-check"></i></Link>
             <Link href={"/dashboard"} className="link-dark text-decoration-none"><h2>LOGO</h2></Link>
             <div className="fs-1 btn px-2 text-danger" data-bs-toggle="modal" data-bs-target="#signoutModal"><i className="bi bi-box-arrow-right"></i></div>
