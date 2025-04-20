98eb34a426f16bc177afc7947229ee4cb0ae2ec0
spidy-verse <eliteanswers.in@gmail.com>
2025-04-20 20:51:35 +0530

    user wallet page carousel banner component updated
    Signed-off-by: spidy-verse <eliteanswers.in@gmail.com>

--- src/app/ui/Others/WalletCarousel.jsx
+++ src/app/ui/Others/WalletCarousel.jsx
@@ -16,17 +16,17 @@ export default function WalletCarousel() {
     return (
         <div id="carouselExampleAutoplaying" className="carousel carousel-fade col" data-bs-ride="carousel">
             <div className="carousel-inner">
-                <div className="carousel-item active" data-bs-interval="2500">
-                    <img src="/wallet-banner-1.png" className="d-block w-100" alt="..." />
+                <div className="carousel-item active" data-bs-interval="3500">
+                    <img src="/wallet-banner-1.webp " className="d-block w-100" alt="Join Telegram" />
                 </div>
-                <div className="carousel-item" data-bs-interval="2500">
-                    <img src="/wallet-banner-2.png" className="d-block w-100" alt="..." />
+                <div className="carousel-item" data-bs-interval="3500">
+                    <img src="/wallet-banner-2.webp " className="d-block w-100" alt="Subscribe Youtube" />
                 </div>
-                <div className="carousel-item" data-bs-interval="2500">
-                    <img src="/wallet-banner-3.png" className="d-block w-100" alt="..." />
+                <div className="carousel-item" data-bs-interval="3500">
+                    <img src="/wallet-banner-3.webp " className="d-block w-100" alt="Join WhatsApp" />
                 </div>
-                <div className="carousel-item" data-bs-interval="2500">
-                    <img src="/wallet-banner-4.png" className="d-block w-100" alt="..." />
+                <div className="carousel-item" data-bs-interval="3500">
+                    <img src="/wallet-banner-4.webp " className="d-block w-100" alt="Follow on Instagram" />
                 </div>
             </div>
         </div>
