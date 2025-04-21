48a56c97d1c67ce0c874687719d301492fdf6599
spidy-verse <eliteanswers.in@gmail.com>
2025-04-21 20:31:53 +0530

    join social networks component seperated
    Signed-off-by: spidy-verse <eliteanswers.in@gmail.com>

---
+++ src/app/ui/Dashboard/JoinSocialNetworks.jsx
@@ -0,0 +1,16 @@
+const JoinSocialNetworks = () => {
+    return (
+        <>
+            <hr />
+            <section className="mt-2 p-2 bg-white d-flex flex-column gap-2">
+                <div className="text-center my-1 fs-5 fw-semibold">Join Our Social Media Networks</div>
+                <img className="w-100" src="/join-telegram.webp" alt="Join Telegram" />
+                <img className="w-100" src="/join-youtube.webp" alt="Join Youtube" />
+                <img className="w-100" src="/join-whatsapp.webp" alt="Join WhatsApp" />
+                <img className="w-100" src="/join-instagram.webp" alt="Join Instagram" />
+            </section>
+        </>
+    );
+}
+
+export default JoinSocialNetworks;
--- src/app/ui/Dashboard/UserDashboard.jsx
+++ src/app/ui/Dashboard/UserDashboard.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import { getAllOffers } from "db/offers/handler";
 import Link from "next/link";
 import { useSearchParams, useRouter } from "next/navigation";
+import JoinSocialNetworks from "./JoinSocialNetworks";
 
 // Button label vs actual category value
 const categoryOptions = [
@@ -99,16 +100,7 @@ const UserDashboard = () => {
             }
 
             {selectedCategory === "" && (
-                <>
-                    <hr />
-                    <section className="mt-2 p-2 bg-white d-flex flex-column gap-2">
-                        <div className="text-center my-1 fs-5 fw-semibold">Join Our Social Media Networks</div>
-                        <img className="w-100" src="/join-telegram.webp" alt="Join Telegram" />
-                        <img className="w-100" src="/join-youtube.webp" alt="Join Youtube" />
-                        <img className="w-100" src="/join-whatsapp.webp" alt="Join WhatsApp" />
-                        <img className="w-100" src="/join-instagram.webp" alt="Join Instagram" />
-                    </section>
-                </>
+                <JoinSocialNetworks/>
             )}
         </>
     );
