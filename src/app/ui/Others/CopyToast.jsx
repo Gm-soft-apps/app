e7698b2827efd02e90f67e224b9a2122f193caae
spidy-verse <eliteanswers.in@gmail.com>
2025-04-21 20:20:53 +0530

    ref code copy toast's size decreased
    Signed-off-by: spidy-verse <eliteanswers.in@gmail.com>

--- src/app/ui/Others/CopyToast.jsx
+++ src/app/ui/Others/CopyToast.jsx
@@ -1,8 +1,8 @@
 const CopyToast = (props) => {
     return (
-        <div id="liveToast" className="w-100 py-2 ps-3 toast text-bg-secondary" role="alert" aria-live="assertive" aria-atomic="true">
+        <div id="liveToast" className="w-100 py-1 toast text-bg-secondary" role="alert" aria-live="assertive" aria-atomic="true">
             <div className="d-flex justify-content-center align-items-center">
-                <div className="toast-body fs-5">
+                <div className="toast-body">
                     {props.message}
                 </div>
             </div>
