1189f78eb2186e5babf7a364f940c0f2885293db
spidy-verse <eliteanswers.in@gmail.com>
2025-04-21 20:40:32 +0530

    offers loading 'loading' skeleton added on homepage
    Signed-off-by: spidy-verse <eliteanswers.in@gmail.com>

--- src/app/ui/Dashboard/UserDashboard.jsx
+++ src/app/ui/Dashboard/UserDashboard.jsx
@@ -5,6 +5,7 @@ import { getAllOffers } from "db/offers/handler";
 import Link from "next/link";
 import { useSearchParams, useRouter } from "next/navigation";
 import JoinSocialNetworks from "./JoinSocialNetworks";
+import Loading from "../AuthForm/loading";
 
 // Button label vs actual category value
 const categoryOptions = [
@@ -81,26 +82,27 @@ const UserDashboard = () => {
                 </h2>
             </div>
 
-            {
-                filteredOffers.map((offer, idx) => (
-                    <div className="p-2 bg-white mx-2 rounded my-2 shadow-lg position-relative" key={idx}>
-                        <div className="user-offer-payout bg-warning position-absolute top-0 end-0 px-3 fw-semibold">Earn Flat ₹ {offer.offerPayout}</div>
-                        <div className="row">
-                            <div className="col-3">
-                                <img src={offer.offerLogo} alt={offer.offerName} className="shadow rounded-1 border border-2" width={65} height={65} />
-                            </div>
-                            <div className="col align-self-center">
-                                <h2 className="fs-4 mt-3">{offer.offerName}</h2>
-                            </div>
-                            <div className="fs-6 fw-semibold mt-1">{"※ " + offer.offerTitle}</div>
+            {offers.length === 0 ? (
+                <div className="text-center py-2 bg-white fw-bold"><Loading/></div>
+            ) : filteredOffers.map((offer, idx) => (
+                <div className="p-2 bg-white mx-2 rounded my-2 shadow-lg position-relative" key={idx}>
+                    <div className="user-offer-payout bg-warning position-absolute top-0 end-0 px-3 fw-semibold">Earn Flat ₹ {offer.offerPayout}</div>
+                    <div className="row">
+                        <div className="col-3">
+                            <img src={offer.offerLogo} alt={offer.offerName} className="shadow rounded-1 border border-2" width={65} height={65} />
                         </div>
-                        <Link href={`/dashboard/offer/${offer.id}`} prefetch={true} className="btn btn-primary py-1 mt-1 w-100 fw-semibold">Complete Now</Link>
+                        <div className="col align-self-center">
+                            <h2 className="fs-4 mt-3">{offer.offerName}</h2>
+                        </div>
+                        <div className="fs-6 fw-semibold mt-1">{"※ " + offer.offerTitle}</div>
                     </div>
-                ))
+                    <Link href={`/dashboard/offer/${offer.id}`} prefetch={true} className="btn btn-primary py-1 mt-1 w-100 fw-semibold">Complete Now</Link>
+                </div>
+            ))
             }
 
             {selectedCategory === "" && (
-                <JoinSocialNetworks/>
+                <JoinSocialNetworks />
             )}
         </>
     );
