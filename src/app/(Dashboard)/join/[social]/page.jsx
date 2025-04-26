23a64f688bd5a11f48a915f870a1da57125613ca
spidy-verse <eliteanswers.in@gmail.com>
2025-04-26 20:09:25 +0530

    socialmedia joining page updated
    Signed-off-by: spidy-verse <eliteanswers.in@gmail.com>

--- src/app/(Dashboard)/join/[social]/page.jsx
+++ src/app/(Dashboard)/join/[social]/page.jsx
@@ -1,9 +1,17 @@
-import Link from "next/link";
-import { redirect } from "next/navigation";
+"use client"
 
-const JoinSocialMedia = async ({ params }) => {
-    let { social } = params;
+import Link from "next/link";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const JoinSocialMedia = () => {
+    const params = useParams();
+    const socialNetwork = params.social;
     const networks = ["telegram", "whatsapp", "youtube", "instagram"];
+
+    const router = useRouter();
+    const [image, setImage] = useState("");
+    const [message, setMessage] = useState("");
 
     const socialLinks = {
         telegram: "No Link Available",
@@ -12,33 +20,59 @@ const JoinSocialMedia = async ({ params }) => {
         instagram: "No Link Available",
     }
 
-    if (!networks.includes(social)) {
-        redirect("/dashboard");
+    const socialColors = {
+        telegram: "bg-primary",
+        whatsapp: "bg-success",
+        youtube: "bg-danger",
+        instagram: "ig-color"
     }
 
-    const link = socialLinks[social];
-    social = social.charAt(0).toUpperCase() + social.slice(1);
+    useEffect(() => {
+        if (!networks.includes(socialNetwork)) {
+            router.push("/dashboard");
+        }
+    }, [socialNetwork, router]); // 👈 Dependency array, watch values
+
+    // If socialNetwork is invalid, don't render anything until redirected
+    if (!networks.includes(socialNetwork)) {
+        return null;
+    }
+
+    const link = socialLinks[socialNetwork];
+    const color = socialColors[socialNetwork];
+    const social = socialNetwork.charAt(0).toUpperCase() + socialNetwork.slice(1);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(image)
+        if(!image){
+            setMessage("Please Select Image from Gallery!");
+            return null
+        }
+        
+    }
 
     return (
         <div className="mx-1">
-            <div className="bg-primary text-white py-1 row align-items-center my-2">
+            <div className={`${color} text-white py-1 row align-items-center my-2`}>
                 <Link href="/dashboard" className="col-2 link-light text-decoration-none text-center">
                     <i className="bi bi-arrow-left-circle fs-4"></i>
                 </Link>
-                <div className="col text-center fw-semibold fs-5">Join {social} Get ₹5</div>
+                <div className={`col text-center fw-semibold fs-5`}>Join {social} Get ₹5</div>
             </div>
 
             <div className="bg-white p-2 text-center my-2">
                 <h3>Click on Link Below</h3>
-                <Link className="fw-semibold fs-6 d-block border border-dark rounded-1 my-1 py-1 link-dark text-decoration-none" href={link} target="_blank">{link}</Link>
+                <Link className="fw-semibold fs-6 d-block border border-dark rounded-1 my-2 py-1 link-dark text-decoration-none" href={link} target="_blank">{link}</Link>
             </div>
 
             <div className="bg-white px-1 py-2 text-center">
-                <div className="border border-dark rounded-top-1 p-2">
+                <form onSubmit={handleSubmit} className="border border-dark rounded-top-1 p-2">
                     <label htmlFor="screenshot" className="d-block mb-2 fw-semibold fs-1 h-300px d-flex justify-content-center align-items-center">+</label>
-                    <input type="file" name="screenshot" id="screenshot" accept="image/*" className="d-none" required/>
-                    <button className="fw-semibold py-1 btn btn-outline-dark w-100">Upload Screenshot</button>
-                </div>
+                    <input type="file" name="screenshot" id="screenshot" accept="image/*" className="d-none" onChange={(e)=>{setImage(e.target.value)}} />
+                    <div className="fw-semibold text-danger my-1">{message}</div>
+                    <button type="submit" className="fw-semibold py-1 btn btn-outline-dark w-100">Upload Screenshot</button>
+                </form>
             </div>
         </div>
     );
