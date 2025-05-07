71d5c994e49294c0da2a626b9ad218e8a447c52c
spidyverse <eliteanswers.in@gmail.com>
2025-05-07 14:09:50 +0000

    offer-links db schema added
    Signed-off-by: spidyverse <eliteanswers.in@gmail.com>

---
+++ src/db/offer-links/schema.ts
@@ -0,0 +1,11 @@
+import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const offerLinks = sqliteTable("offer_links", {
+    sl_no: int().primaryKey().notNull(),
+    offer_id: int().notNull(),
+    user_id: int().notNull(),
+    self_path: text().notNull().unique(),
+    share_path: text().notNull().unique(),
+    created_on: int(),
+})
+
