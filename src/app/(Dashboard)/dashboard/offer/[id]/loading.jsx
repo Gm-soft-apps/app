7fd1fba694c27c25cb3c7ddc83b9ccaf865ea588
spidyverse <eliteanswers.in@gmail.com>
2025-05-08 06:10:38 +0000

    offer page loading code updated
    Signed-off-by: spidyverse <eliteanswers.in@gmail.com>

--- src/app/(Dashboard)/dashboard/offer/[id]/loading.jsx
+++ src/app/(Dashboard)/dashboard/offer/[id]/loading.jsx
@@ -2,7 +2,7 @@ import Loading from "app/ui/AuthForm/loading";
 
 const OfferPageLoading = ()=>{
     return (
-        <div className="h-100 bg-white d-flex justify-content-center align-items-center">
+        <div className="bg-white d-flex justify-content-center align-items-center">
             <Loading/>
         </div>
     );
