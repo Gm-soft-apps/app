a2a5b0910d18cf2a2a97172b9586ec995385557f
spidy-verse <eliteanswers.in@gmail.com>
2025-05-09 21:16:34 +0530

    offer path redirected to afflink with params
    Signed-off-by: spidy-verse <eliteanswers.in@gmail.com>

--- src/app/(ShortLinks)/[slug]/route.js
+++ src/app/(ShortLinks)/[slug]/route.js
@@ -1,22 +1,25 @@
 import { searchOfferPath } from "db/offer-paths/handler";
 import { getOfferByID } from "db/offers/handler";
-import { redirect } from "next/navigation";
 import { NextResponse } from "next/server";
 
 export const GET = async (request, { params }) => {
     const { slug } = await params;
     const headers = request.headers;
 
-    if (slug.length !== 6) {
-        redirect("/register")
+    const offerPathObj = await searchOfferPath(slug);
+
+    if (!offerPathObj) {
+        return NextResponse.redirect(new URL("/register", request.url))
     }
 
-    const offerPathObj = await searchOfferPath(slug);
     const offerID = offerPathObj.offer_id;
     const userID = offerPathObj.user_id;
-    const offer = await getOfferByID(offerID)
+    const offer = await getOfferByID(offerID);
+    let affLink = offer.offerLink;
 
-    // redirect(offer.offerLink)
+    if (offer.affNetwork === "monetizedeal") {
+        affLink = `${offer.offerLink}?subid1=${userID}&subid2=${offerID}&subid3=${slug}&subid4=${null}&subid5=${null}`
+    }
 
-    return NextResponse.redirect(offer.offerLink)
+    return NextResponse.redirect(affLink)
 }
